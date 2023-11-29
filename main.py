20da291af27c6f4da4fcfa458adce648f4516053
kiitoss <antoinechataignier@gmail.com>
2023-11-28 22:38:47 -0500

    feat: clean

--- main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from backend.api import app
-
-if __name__ == '__main__':
-    # Run the application
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-
-    # Uvicorn will be runnning on http://localhost:8000 (Press CTRL+C to quit)
